285a531911738006b27dde43cb22f552dbe75f55
alfsuace <alfonso.suaace@educa.jcyl.es>
2024-02-21 16:43:06 +0100

    Ejercicio 2: rooom -converter

--- app/src/main/java/com/iesam/ex_22_23_aad_marzo/feature/animals/data/local/Converters.kt
+++ app/src/main/java/com/iesam/ex_22_23_aad_marzo/feature/animals/data/local/Converters.kt
@@ -2,7 +2,7 @@ package com.iesam.ex_22_23_aad_marzo.feature.animals.data.local
 
 import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
-import com.google.gson.annotations.SerializedName
+import com.google.gson.Gson
 import com.iesam.ex_22_23_aad_marzo.feature.animals.domain.AnimalType
 
 @ProvidedTypeConverter
@@ -14,6 +14,6 @@ class Converters {
 
     @TypeConverter
     fun toAnymalType(animal: String): AnimalType {
-        SerializedName(animal) to AnimalType()
+        return Gson().fromJson(animal, AnimalType::class.java)
     }
 }
