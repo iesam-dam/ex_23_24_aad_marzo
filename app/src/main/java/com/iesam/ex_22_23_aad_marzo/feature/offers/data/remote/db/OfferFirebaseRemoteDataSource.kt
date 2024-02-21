6d10eb6d01013e0c72997bb43d211dc6b391eb01
HectorMartin4 <hectormartin4.c@gmail.com>
2024-02-21 18:55:07 +0100

    Fixed

--- app/src/main/java/com/iesam/ex_22_23_aad_marzo/feature/offers/data/remote/db/OfferFirebaseRemoteDataSource.kt
+++ app/src/main/java/com/iesam/ex_22_23_aad_marzo/feature/offers/data/remote/db/OfferFirebaseRemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.iesam.ex_22_23_aad_marzo.feature.offers.data.remote.db
 import com.google.firebase.Firebase
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.database
+import com.iesam.ex_22_23_aad_marzo.feature.offers.data.OfferDataRepository
 import com.iesam.ex_22_23_aad_marzo.feature.offers.domain.Offer
 import kotlinx.coroutines.tasks.await
 
@@ -11,8 +12,8 @@ class OfferFirebaseRemoteDataSource {
     private val database = Firebase.database
 
     suspend fun getOffers(): List<Offer>{
-        database.getReference("offer").get().await().children.map {
-
+        return database.getReference("offer").get().await().children.map {
+            it.getValue(OfferFirebaseModel::class.java)!!.toDomain()
         }
     }
 
